8833657b463ab41bf05f9bc0c41d2e63fafd92a2
Frits <sendie61@hotmail.com>
2022-08-10 14:34:09 +0200

    staged adxded

--- TSTurning/main.cpp
+++ TSTurning/main.cpp
@@ -1,11 +1,26 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QCommandLineParser>
+
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+    QApplication app(argc, argv);
+
+    QCoreApplication::setOrganizationName("Tech-Skill");
+    QCoreApplication::setApplicationName("TSTurning");
+    QCoreApplication::setApplicationVersion(QT_VERSION_STR);
+    QCommandLineParser parser;
+    parser.setApplicationDescription(QCoreApplication::applicationName());
+    parser.addHelpOption();
+    parser.addVersionOption();
+    parser.addPositionalArgument("file", "The file to open.");
+    parser.process(app);
+
+    MainWindow mainWin;
+    if (!parser.positionalArguments().isEmpty())
+        mainWin.loadFile(parser.positionalArguments().first());
+    mainWin.show();
+    return app.exec();
 }
